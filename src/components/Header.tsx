148763622e6f965ce532de531ed6e9955e43aa06
nikisiagit <n.ovcinnikovs@rgu.ac.uk>
2026-01-30 13:26:24 +0000

    Animate burger menu icon from lines to cross

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -88,18 +88,9 @@ export default function Header({ events }: HeaderProps) {
                         aria-expanded={isMenuOpen}
                     >
                         <svg width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                            {isMenuOpen ? (
-                                <>
-                                    <line x1="18" y1="6" x2="6" y2="18"></line>
-                                    <line x1="6" y1="6" x2="18" y2="18"></line>
-                                </>
-                            ) : (
-                                <>
-                                    <line x1="3" y1="12" x2="21" y2="12"></line>
-                                    <line x1="3" y1="6" x2="21" y2="6"></line>
-                                    <line x1="3" y1="18" x2="21" y2="18"></line>
-                                </>
-                            )}
+                            <line className="line-top" x1="3" y1="6" x2="21" y2="6"></line>
+                            <line className="line-middle" x1="3" y1="12" x2="21" y2="12"></line>
+                            <line className="line-bottom" x1="3" y1="18" x2="21" y2="18"></line>
                         </svg>
                     </button>
                 </div>
