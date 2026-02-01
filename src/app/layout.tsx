1b465b3db9d69b6306e6080a49df6c1aaf4b3c5b
nikisiagit <n.ovcinnikovs@rgu.ac.uk>
2026-02-01 09:23:36 +0000

    Analytics: Add Google Analytics (gtag.js)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Archivo, Archivo_Black } from "next/font/google";
 import "./globals.css";
 
@@ -26,6 +27,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${archivo.variable} ${archivoBlack.variable}`}>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-JCWTLQ4F23"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-JCWTLQ4F23');
+          `}
+        </Script>
         {children}
       </body>
     </html>
