b23835ad2cffc392431627a5236b03fd176db352
nikisiagit <n.ovcinnikovs@rgu.ac.uk>
2026-02-01 09:30:59 +0000

    Analytics: Add Microsoft Clarity tracking (privacy compliant)

--- src/components/CookieBanner.tsx
+++ src/components/CookieBanner.tsx
@@ -48,6 +48,15 @@ export default function CookieBanner() {
               gtag('config', '${GA_MEASUREMENT_ID}');
             `}
                     </Script>
+                    <Script id="clarity-script" strategy="afterInteractive">
+                        {`
+                            (function(c,l,a,r,i,t,y){
+                                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                            })(window, document, "clarity", "script", "vai4iz9ei8");
+                        `}
+                    </Script>
                 </>
             )}
 
