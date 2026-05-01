1e59ea31ea6ad6f0343053473800f1a60a47d1c0
Nikita Ovcinnikovs <nikitaovcinnikovs@Nikitas-MBP.local>
2026-05-01 12:44:19 +0100

    Add Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,7 +65,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-53JXHLF6');
+        `}
+      </Script>
       <body className={`${archivo.variable} ${archivoBlack.variable}`}>
+        <noscript dangerouslySetInnerHTML={{ __html: \`<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-53JXHLF6" height="0" width="0" style="display:none;visibility:hidden"></iframe>\`}} />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
